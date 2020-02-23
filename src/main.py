539fb6a14f85ea66248de4b6c0c685df54fd4603
Rupert Adams <RupertNCAdams@aol.com>
2020-02-23 13:24:46 +0000

    began exploring the dataset

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import requests
+import pprint
 
 request = requests.get('https://api.tfl.gov.uk/line/24/route/sequence/outbound')
 route_json = request.json()
-print(route_json['lineName'])
+
+for p in route_json['stations']:
+    for l in p['lines']:
+        if p['lines'][0]['id'] == '24':
+            print(p['name'])
+
+# pprint.pprint(route_json['stations'])
