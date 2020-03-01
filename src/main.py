a3ac14893db0c0b66bdfe15a84440e0ebde1c75b
Rupert Adams <RupertNCAdams@aol.com>
2020-03-01 19:26:53 +0000

    added line 127 id finder

--- src/main.py
+++ src/main.py
@@ -2,7 +2,13 @@ import requests
 import pprint
 
 request = requests.get('https://api.tfl.gov.uk/StopPoint/490014138S/Arrivals')
+stop_points = requests.get('https://api.tfl.gov.uk/Line/127/StopPoints')
 route_json = request.json()
+stoppoint_codes = stop_points.json()
+
+for s in stoppoint_codes:
+    print(s['stationNaptan'])
+    print(s['commonName'])
 
 for p in route_json:
     if p['expectedArrival'] <= p['timeToLive']:
@@ -16,4 +22,4 @@ for p in route_json:
         print(f"towards {p['towards']}")
         print('is late')
 
-# pprint.pprint(route_json)
+# pprint.pprint(stoppoint_codes)
