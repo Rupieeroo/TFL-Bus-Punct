c37578d4a3f8bc0ffce42b0370568bef4530b957
Rupert Adams <RupertNCAdams@aol.com>
2020-03-08 17:07:07 +0000

    not working yet, need to figure out how to populate like this

--- src/main.py
+++ src/main.py
@@ -1,26 +1,46 @@
+#!/usr/bin/env python3
+
 import requests
-import pprint
+import csv
+from dataclasses import dataclass, asdict
+from sys import stdout
 
 request = requests.get('https://api.tfl.gov.uk/StopPoint/490014138S/Arrivals')
-stop_points = requests.get('https://api.tfl.gov.uk/Line/127/StopPoints')
 route_json = request.json()
-stoppoint_codes = stop_points.json()
 
-for s in stoppoint_codes:
-    print(s['stationNaptan'])
-    print(s['commonName'])
-    print(f"towards {s['additionalProperties'][1]['value']}")
 
-for p in route_json:
-    if p['expectedArrival'] <= p['timeToLive']:
-        print(f"The {p['lineName']}")
-        print(f"arriving at {p['stationName']}")
-        print(f"towards {p['towards']}")
-        print('is on time')
-    else:
-        print(f"The {p['lineName']}")
-        print(f"arriving at {p['stationName']}")
-        print(f"towards {p['towards']}")
-        print('is late')
+@dataclass
+class Stop:
+    route_no = int
 
-# pprint.pprint(stoppoint_codes)
+    def stop_points(self):
+        stop_point = requests.get(f'https://api.tfl.gov.uk/Line/{self.route_no}/StopPoints')
+        stoppoint_codes = stop_point.json()
+
+        for s in stoppoint_codes:
+            yield s['stationNaptan']
+            yield s['commonName']
+            yield s['additionalProperties'][1]['value']
+
+
+def route_finder():
+    for p in route_json:
+        if p['expectedArrival'] <= p['timeToLive']:
+            print(f"The {p['lineName']}")
+            print(f"arriving at {p['stationName']}")
+            print(f"towards {p['towards']}")
+            print('is on time')
+        else:
+            print(f"The {p['lineName']}")
+            print(f"arriving at {p['stationName']}")
+            print(f"towards {p['towards']}")
+            print('is late')
+
+
+if __name__ == "__main__":
+    data_table = csv.DictWriter(
+        stdout, ['Station_naptan', 'Name', 'Direction']
+    )
+    data_table.writeheader()
+    for row in Stop().stop_points(24):
+        data_table.writerow(asdict(row))
